eb61d13a03b707c6951747fa03f4a8968b291e8e
Jakob <WintersteigerJ190225@sus.htl-grieskirchen.at>
2024-06-08 18:37:49 +0200

    vvvvvvvvvvvvvvvvvvvvv

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    <slot />
+  <div class="drawer drawer-open">
+    <input
+      id="my-drawer-2"
+      type="checkbox"
+      class="drawer-toggle"
+    >
+    <div class="drawer-content">
+      <!-- Page content here -->
+      <slot />
+    </div>
+    <div class="drawer-side">
+      <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <!-- Sidebar content here -->
+        <li>
+          <a @click="navigateTo({ path: '/' })">
+            Sidebar Item 1
+          </a>
+        </li>
+        <li>
+          <a @click="navigateTo({ path: '/timetables' })">
+            Sidebar Item 2
+          </a>
+        </li>
+        <li :class="{ 'bg-black': currentRoute.path === '/timetables' }">
+          <a @click="navigateTo({ path: '/timetables' })">Item 3</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const router = useRouter()
+
+const { currentRoute } = router
+
+watch(currentRoute, () => {
+  console.log(currentRoute.value.path)
+})
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,30 +1,9 @@
 <template>
-  <div class="drawer drawer-open">
-    <input
-      id="my-drawer-2"
-      type="checkbox"
-      class="drawer-toggle"
-    >
-    <div class="drawer-content flex flex-col items-center justify-center">
-      <!-- Page content here -->
-      Content
-    </div>
-    <div class="drawer-side">
-      <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-        <!-- Sidebar content here -->
-        <li>
-          <a>Sidebar Item 1</a>
-        </li>
-        <li>
-          <a>Sidebar Item 2</a>
-        </li>
-      </ul>
-    </div>
+  <div>
+    Content1
   </div>
 </template>
 
 <script setup lang="ts">
-onMounted(() => {
-  console.log('mounted')
-})
+
 </script>
